286aa585034d6cc626a73eccf5f6be7638acb7b6
Lothar Komas <thomaskolar90@gmail.com>
2016-04-24 11:39:07 +0200

    add mail to setup script

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='wurstmineberg-common-python',
       version='0.1',
       description='Common functionality for Wurstmineberg python infrastructure',
       author='Wurstmineberg',
+      author_email='mail@wurstmineberg.de',
       py_modules=["wmb"],
      )
 
