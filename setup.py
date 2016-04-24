794b8c4282b6437832395edcb44253b3218d8680
Lothar Komas <thomaskolar90@gmail.com>
2016-04-24 14:32:04 +0200

    use versioneer for versioning

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import versioneer
 
 setup(name='wurstmineberg-common-python',
-      version='0.1',
+      cmdclass=versioneer.get_cmdclass(),
+      version=versioneer.get_version(),
       description='Common functionality for Wurstmineberg python infrastructure',
       author='Wurstmineberg',
       author_email='mail@wurstmineberg.de',
-      py_modules=["wmb"],
+      py_modules=["wmb", "_version"],
      )
 
